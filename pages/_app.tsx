cca2db66720013f62c3d81b5d0eeeeaf072e2ef9
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-17 19:54:06 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,14 @@
 import App from 'next/app';
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { themeDark, themeLight } from 'lib/theme';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={false ? themeDark : themeLight}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
