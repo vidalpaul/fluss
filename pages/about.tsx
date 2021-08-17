29dfce27758e6ef367f2fed6e0d335019eade9d6
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-17 20:46:18 -0300

    some routing

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Container, Typography, Box, Button } from '@material-ui/core';
 import Link from 'next/link';
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth='sm'>
       <Box my={4}>
         <Typography variant='h4' component='h1' gutterBottom>
           Next.js example
         </Typography>
-        <Link href='/about'>
+        <Link href='/'>
           <Button variant='contained' color='primary'>
-            Go to the about page
+            Go to the index page
           </Button>
         </Link>
       </Box>
