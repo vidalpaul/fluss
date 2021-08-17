7d2e635f16fd29a8d498090e5e7a23b2d66fe0c5
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-08-17 20:30:22 -0300

    Remove the server side injected CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
-import App from 'next/app';
+import { useEffect } from 'react';
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { themeDark, themeLight } from 'lib/theme';
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server side injected CSS
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <ThemeProvider theme={false ? themeDark : themeLight}>
       <CssBaseline />
